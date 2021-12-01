d8594b59c86daf6345fb52c1b834f1f79f02a978
JanitaA <59653594+JanitaA@users.noreply.github.com>
2021-12-01 10:54:12 +0200

    Cost-ominaisuus poistettu

--- tuasworkspaces/populate.py
+++ tuasworkspaces/populate.py
@@ -44,54 +44,28 @@ user.set_password('george')
 db.session.add(user)
 
 # add rooms
-room=Room(id=1,roomName='room1',telephone=True,projector=True,whiteboard=True,cost=500) # POISTA COST
+room=Room(id=1,roomName='room1',telephone=True,projector=True,whiteboard=True) # POISTA COST
 db.session.add(room)
-room=Room(id=2,roomName='room2',telephone=False,projector=True,whiteboard=True,cost=300)
+room=Room(id=2,roomName='room2',telephone=False,projector=True,whiteboard=True)
 db.session.add(room)
-room=Room(id=3,roomName='room3',telephone=False,projector=False,whiteboard=True,cost=100)
+room=Room(id=3,roomName='room3',telephone=False,projector=False,whiteboard=True)
 db.session.add(room)
-room=Room(id=4,roomName='room4',telephone=False,projector=False,whiteboard=True,cost=100)
+room=Room(id=4,roomName='room4',telephone=False,projector=False,whiteboard=True)
 db.session.add(room)
 
 # add past meetings
 meeting=Meeting(id=9,title='past meeting',teamId=3,roomId=2,bookerId=2,date=datetime(2018,2,15),startTime=10,endTime=14,duration=4)
 db.session.add(meeting)
 
-# add past meeting cost log
-costlog=CostLog(id=10,teamId=4,teamName='Dev',title='past meeting',date=datetime(2018,2,15),cost=1200)
-db.session.add(costlog)
-
-# add business partners
-partner=Businesspartner(id=1,name='Zeus',representing='Thunder electricity',position='CEO')
-db.session.add(partner)
-partner=Businesspartner(id=2,name='Yan',representing='Facebook',position='CTO')
-db.session.add(partner)
-partner=Businesspartner(id=3,name='Xanos',representing='Avengers',position='Boss')
-db.session.add(partner)
-
 # add past meetings
 meeting=Meeting(id=15,title='future meeting1',teamId=3,roomId=2,bookerId=2,date=datetime(2018,3,15),startTime=11,endTime=14,duration=3)
 db.session.add(meeting)
 
-# add past meeting cost log
-costlog=CostLog(id=15,teamId=3,teamName='Ops',title='future meeting1',date=datetime(2018,3,15),cost=900)
-db.session.add(costlog)
-
 meeting=Meeting(id=16,title='future meeting2',teamId=6,roomId=3,bookerId=6,date=datetime(2018,3,15),startTime=10,endTime=13,duration=3)
 db.session.add(meeting)
 
-# add past meeting cost log
-costlog=CostLog(id=16,teamId=6,teamName='Market',title='future meeting2',date=datetime(2018,3,15),cost=300)
-db.session.add(costlog)
-
 meeting=Meeting(id=17,title='future meeting3',teamId=6,roomId=3,bookerId=6,date=datetime(2018,3,15),startTime=15,endTime=19,duration=4)
 db.session.add(meeting)
-
-# add past meeting cost log
-costlog=CostLog(id=17,teamId=6,teamName='Market',title='future meeting3',date=datetime(2018,3,15),cost=400)
-db.session.add(costlog)
-
-
 
 participants_user=Participants_user(id=1,meeting='future meeting1',userId=2)
 db.session.add(participants_user)
