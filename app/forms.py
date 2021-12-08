7459e4d0ee95e23420f3fbab5c91da24a383a456
JanitaA <59653594+JanitaA@users.noreply.github.com>
2021-12-08 14:55:51 +0200

    Muokkausta

--- app/forms.py
+++ app/forms.py
@@ -109,11 +109,11 @@ class RoomChoiceIterable(object):
             yield choice
 
 class BookmeetingForm(FlaskForm):
-    title=StringField('Meeting title',validators=[DataRequired()])
+    title=StringField('Meeting title')
     rooms=SelectField('Choose room',coerce=int,choices=RoomChoiceIterable())
     date=DateField('Choose date', format="%m/%d/%Y",validators=[DataRequired()])
-    startTime=SelectField('Choose starting time(in 24hr expression)',coerce=int,choices=[(i,i) for i in range(9,19)])
-    duration=SelectField('Choose duration of the meeting(in hours)',coerce=int,choices=[(i,i) for i in range(1,6)])
+    startTime=SelectField('Choose starting time(in 24hr expression)',coerce=int,choices=[(i,i) for i in range(16,21)])
+    duration=SelectField('Choose duration of the meeting(in hours)',coerce=int,choices=[(i,i) for i in range(1,5)])
     participants_user=SelectMultipleField('Choose participants from company',coerce=int,choices=UserChoiceIterable(),option_widget=widgets.CheckboxInput(),widget=widgets.ListWidget(prefix_label=False),validators=[DataRequired()])
     participants_partner=SelectMultipleField('Choose participants from partners',coerce=int,choices=PartnerChoiceIterable(),option_widget=widgets.CheckboxInput(),widget=widgets.ListWidget(prefix_label=False))
     submit=SubmitField('Book')
