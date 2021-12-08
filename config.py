cdb7eacb98c40e9f145dbf2e8a0653935347d300
annahuhtanen <anna.huhtanen@edu.turkuamk.fi>
2021-12-08 16:15:23 +0200

    database update2

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'lab2.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     DEBUG = True
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:anna@localhost/TuasWorkspaces'
